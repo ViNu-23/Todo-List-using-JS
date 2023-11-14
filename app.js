c3753150b72321bd4985240c1c552a8223fa51b6
ViNu-23 <vijayvinu46@gmail.com>
2023-11-15 02:08:53 +0530

    ui fix and rendering

--- app.js
+++ app.js
@@ -1,68 +1,63 @@
 const todoInput = document.querySelector("#task-input");
 const todoButton = document.querySelector("#task-submit");
-const todoList = document.querySelector(".task-list");
+const todoList = document.querySelector(".card");
 
 todoButton.addEventListener("click", addTodo);
 
-
+//null validation
 function addTodo(e) {
-  if((todoInput.value)==0){
-    alert('Enter Task')
-  }else{
-    addTodo2(e)
+  if (todoInput.value == 0) {
+    alert("Enter Task");
+  } else {
+    goodToGo(e);
   }
 }
 
-function addTodo2(e) {
-  //created li list
- 
-  const divList = document.createElement("div");
-  divList.className = "input-group input-group-lg";
-  
+function goodToGo(e) {
+  const mainDiv = document.createElement("div");
+  mainDiv.className = "card-body";
+  todoList.appendChild(mainDiv);
 
+  const paraElement = document.createElement("p");
+  paraElement.className = "card-text";
+  paraElement.appendChild(document.createTextNode(todoInput.value));
+  paraElement.value = "";
+  mainDiv.appendChild(paraElement);
 
-  const todoLi = document.createElement("li");
-  todoLi.className = " list-group-item";
-  todoLi.appendChild(document.createTextNode(todoInput.value));
-  todoLi.value = "";
-  divList.appendChild(todoLi);
-
-  //creating task-complete button
+  //task complete button rendering
   const completeBtn = document.createElement("button");
   completeBtn.className = "btn btn-primary";
   completeBtn.innerHTML = `Completed`;
-  divList.appendChild(completeBtn);
+  mainDiv.appendChild(completeBtn);
 
-  //creating task-remove button
+  //task remove button rendering
   const trashBtn = document.createElement("button");
   trashBtn.className = "btn btn-danger";
   trashBtn.innerHTML = `Delete`;
-  divList.appendChild(trashBtn);
+  mainDiv.appendChild(trashBtn);
 
-  
-  todoList.appendChild(divList);
   e.preventDefault();
 }
 
 //remove task from task list
-const removeTask = document.querySelector(".task-list");
-removeTask.addEventListener("click", remTask);
+// const removeTask = document.querySelector(".task-list");
+// removeTask.addEventListener("click", remTask);
 
-function remTask(e) {
-  const items = e.target;
-  if (items.className === "btn btn-danger") {
-    const todo = items.parentElement;
-    todo.classList.add("fall");
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
-    });
-    //task completed effect
-  }
-  if (items.className === "btn btn-primary") {
-    const todo = items.parentElement;
-    todo.classList.toggle("completed");
-    result = todoInput.value;
-  }
+// function remTask(e) {
+//   const items = e.target;
+//   if (items.className === "btn btn-danger") {
+//     const todo = items.parentElement;
+//     todo.classList.add("fall");
+//     todo.addEventListener("transitionend", function () {
+//       todo.remove();
+//     });
+//     //task completed effect
+//   }
+//   if (items.className === "btn btn-primary") {
+//     const todo = items.parentElement;
+//     todo.classList.toggle("completed");
+//     result = todoInput.value;
+//   }
 
-  e.preventDefault();
-}
+//   e.preventDefault();
+// }
